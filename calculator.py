c4f41cb57fe94dc5a1c30a3a223fea90fb07d899
Sadie <sadie.spellman@gmail.com>
2025-11-12 11:08:49 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -12,24 +12,12 @@ def square_root(a):
     return math.sqrt(a)
 def hypotenuse(a,b):
     return math.hypot(a,b)
-def add(a, b): 
-    return a+b
-def sub(a,b):
-    return a-b
-def mul(a,b):
-    return a*b
-def div(a,b):
-    if a == 0:
-        raise ZeroDivisionError
-    else:
-        return b/a
 def log(a,b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError
     return math.log(b, a)
 def exp(a,b):
     return a**b
-
 def add(a,b):
     return a+b
 def sub(a,b):
@@ -41,10 +29,3 @@ def div(a,b):
         raise ZeroDivisionError
     else:
         return b/a
-def log(a,b):
-    if b<=0:
-        raise ValueError
-    else:
-        return math.log(a,b)
-def exp(a,b):
-    return pow(a,b)
