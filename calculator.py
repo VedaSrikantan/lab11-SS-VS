de277f5729d8a3f5c5f08d54a111a65d9fd7a4de
Sadie <sadie.spellman@gmail.com>
2025-11-12 14:45:17 -0500

    fixed order of functions

--- calculator.py
+++ calculator.py
@@ -10,18 +10,6 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def square_root(a):
-    if a<0:
-        raise ValueError
-    return math.sqrt(a)
-def hypotenuse(a,b):
-    return math.hypot(a,b)
-def log(a,b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError
-    return math.log(b, a)
-def exp(a,b):
-    return a**b
 def add(a,b):
     return a+b
 def sub(a,b):
@@ -33,3 +21,15 @@ def div(a,b):
         raise ZeroDivisionError
     else:
         return b/a
+def log(a,b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+def exp(a,b):
+    return a**b
+def square_root(a):
+    if a<0:
+        raise ValueError
+    return math.sqrt(a)
+def hypotenuse(a,b):
+    return math.hypot(a,b)
